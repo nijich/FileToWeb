c9d8e6a43a5f59e5069fb16aee98539e4520b21f
nijic <nijich@163.com>
2021-03-27 12:40:44 +0800

    optimize layout, add icon

--- walk.go
+++ walk.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 type UI struct {
@@ -27,10 +28,12 @@ type UI struct {
 	server   *http.Server
 }
 
-func (ui *UI) checkPort() (e string) {
+func (ui *UI) check() (e string) {
 	port := ui.port.Text()
 	num, err := strconv.Atoi(port)
-	if err != nil || num < 1 || num > 65535 {
+	if strings.Trim(ui.path.Text(), " ") == "" {
+		e = "无效的路径"
+	} else if err != nil || num < 1 || num > 65535 {
 		e = "无效的端口号"
 	} else {
 		e = ""
@@ -41,11 +44,9 @@ func (ui *UI) checkPort() (e string) {
 
 func (ui *UI) Start() {
 
-	errString := ui.checkPort()
+	errString := ui.check()
 	if errString != "" {
-		ui.status.Synchronize(func() {
-			ui.status.SetText("错误：" + errString)
-		})
+		ui.status.SetText("错误：" + errString)
 		return
 	}
 
@@ -66,7 +67,15 @@ func (ui *UI) Start() {
 	ui.e.base = ui.path.Text()
 
 	ui.server = &http.Server{Addr: ui.e.port, Handler: ui.e}
-	go ui.server.ListenAndServe()
+	var err error
+	go func() {
+		err = ui.server.ListenAndServe()
+	}()
+	if err != nil {
+		ui.status.SetText("错误：启动服务器失败")
+		ui.btnStart.SetEnabled(true)
+		ui.btnClose.SetEnabled(false)
+	}
 }
 
 func (ui *UI) Close() {
@@ -87,55 +96,135 @@ func createWindow() UI {
 		port: ":9999",
 		auth: false,
 	}
+	icon, _ := walk.NewIconFromFile("app.ico")
 
 	window := declarative.MainWindow{
 		AssignTo: &ui.window,
 		Title:    "网络文件浏览器",
-		MinSize:  declarative.Size{Width: 200, Height: 100},
-		Size:     declarative.Size{Width: 200, Height: 200},
-		Layout:   declarative.VBox{},
+		Icon:     icon,
+		MinSize:  declarative.Size{Width: 600, Height: 400},
+		Size:     declarative.Size{Width: 600, Height: 400},
+		//Bounds: declarative.Rectangle{
+		//	X: 600,
+		//	Y: 200,
+		//},
+		Layout: declarative.VBox{},
 		Children: []declarative.Widget{
-			declarative.TextLabel{
-				AssignTo: &ui.status,
-				Text:     "状态：关闭",
-			},
-			declarative.LineEdit{
-				AssignTo:  &ui.path,
-				CueBanner: "输入路径",
-			},
-			declarative.LineEdit{
-				AssignTo:  &ui.port,
-				CueBanner: "输入使用的端口号",
-				//OnEditingFinished: ui.RefreshQRCode,
-			},
-			declarative.PushButton{
-				AssignTo:  &ui.btnStart,
-				Text:      "开启服务并生成二维码",
-				OnClicked: ui.Start,
-			},
-			declarative.PushButton{
-				AssignTo:  &ui.btnClose,
-				Text:      "关闭服务",
-				OnClicked: ui.Close,
-			},
-			declarative.ComboBox{
-				AssignTo: &ui.addrs,
-				//OnCurrentIndexChanged: ui.RefreshQRCode, // seems invoke three times once
-			},
-			declarative.ImageView{
-				AssignTo: &ui.qrcode,
-			},
-			declarative.CheckBox{
-				AssignTo: &ui.auth,
-				Text:     "启用密码验证",
-			},
-			declarative.LineEdit{
-				AssignTo:  &ui.username,
-				CueBanner: "默认为空",
-			},
-			declarative.LineEdit{
-				AssignTo:  &ui.password,
-				CueBanner: "默认为空",
+
+			declarative.Composite{
+				Layout: declarative.Grid{},
+				Children: []declarative.Widget{
+					declarative.Composite{
+						Row:    0,
+						Column: 0,
+						Layout: declarative.Grid{},
+						Children: []declarative.Widget{
+							declarative.TextLabel{
+								Text:   "本地路径:",
+								Row:    0,
+								Column: 0,
+							},
+							declarative.LineEdit{
+								AssignTo:  &ui.path,
+								CueBanner: "例如: E:\\",
+								Row:       0,
+								Column:    1,
+							},
+							declarative.TextLabel{
+								Text:   "端口号:",
+								Row:    1,
+								Column: 0,
+							},
+							declarative.LineEdit{
+								AssignTo:  &ui.port,
+								CueBanner: "例如: 9999",
+								Row:       1,
+								Column:    1,
+								//OnEditingFinished: ui.RefreshQRCode,
+							},
+
+							declarative.CheckBox{
+								AssignTo: &ui.auth,
+								Text:     "启用密码验证",
+								Row:      2,
+								Column:   0,
+							},
+							declarative.TextLabel{
+								Text:   "用户名:",
+								Row:    3,
+								Column: 0,
+							},
+							declarative.LineEdit{
+								AssignTo:  &ui.username,
+								CueBanner: "默认为空",
+								Row:       3,
+								Column:    1,
+							},
+							declarative.TextLabel{
+								Text:   "密码:",
+								Row:    4,
+								Column: 0,
+							},
+							declarative.LineEdit{
+								AssignTo:  &ui.password,
+								CueBanner: "默认为空",
+								Row:       4,
+								Column:    1,
+							},
+
+							declarative.Composite{
+								Row:        5,
+								Column:     0,
+								ColumnSpan: 2,
+								Layout:     declarative.HBox{},
+								Children: []declarative.Widget{
+
+									declarative.PushButton{
+										AssignTo:  &ui.btnStart,
+										Text:      "开启服务并生成二维码",
+										OnClicked: ui.Start,
+										MinSize:   declarative.Size{150, 10},
+									},
+									declarative.HSpacer{},
+									declarative.PushButton{
+										AssignTo:  &ui.btnClose,
+										Text:      "关闭服务",
+										OnClicked: ui.Close,
+										MinSize:   declarative.Size{150, 10},
+									},
+								},
+							},
+						},
+					},
+					declarative.Composite{
+						Background: declarative.SolidColorBrush{walk.RGB(255, 255, 255)},
+						Row:        0,
+						Column:     1,
+						Layout:     declarative.VBox{},
+						Children: []declarative.Widget{
+							declarative.Label{
+								Text: "二维码使用的本地地址:",
+							},
+							declarative.ComboBox{
+								AssignTo: &ui.addrs,
+								//OnCurrentIndexChanged: ui.RefreshQRCode, // seems invoke three times once
+							},
+							declarative.ImageView{
+								MinSize:  declarative.Size{200, 200},
+								AssignTo: &ui.qrcode,
+							},
+						},
+					},
+
+					declarative.TextLabel{
+						AssignTo:      &ui.status,
+						Text:          "状态：关闭",
+						Row:           1,
+						Column:        0,
+						ColumnSpan:    2,
+						TextAlignment: declarative.AlignHCenterVCenter,
+					},
+				},
 			},
 		},
 	}
@@ -154,11 +243,13 @@ func (ui *UI) initInfo() {
 	ui.addrs.SetModel(addrs)
 	ui.addrs.SetCurrentIndex(0)
 	ui.btnClose.SetEnabled(false)
+	ui.qrcode.SetImage(genQRCode(" ", ""))
 }
 
 func main() {
 
 	ui := createWindow()
+
 	ui.initInfo()
 	ui.window.Run()
 	//x := getLocalIP()
@@ -192,10 +283,10 @@ func getLocalIP() (result []string) {
 }
 
 func genQRCode(ip string, port string) walk.Image {
-	qr, err := qrcode.New("http://"+ip+port, qrcode.Medium)
+	qr, err := qrcode.New("http://"+ip+port, qrcode.High)
 	if err != nil {
 		return nil
 	}
-	image, _ := walk.NewBitmapFromImageForDPI(qr.Image(1000), 1000)
+	image, _ := walk.NewBitmapFromImageForDPI(qr.Image(1000), 500)
 	return image
 }
